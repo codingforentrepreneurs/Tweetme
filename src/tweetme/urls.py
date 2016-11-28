683fa12b6416e830e472d9c270e150be46e3e7c9
CFE <hello@teamcfe.com>
2016-11-28 15:55:05 -0800

    7 - View Function & Templates

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
 
 
