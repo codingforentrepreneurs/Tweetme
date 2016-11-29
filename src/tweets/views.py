0c9a2330b83add7877ff2c6744e17b75fe6eb2b4
CFE <hello@teamcfe.com>
2016-11-28 17:40:05 -0800

    13 - Querying the Database & Context

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
 
 # Create
@@ -13,8 +14,20 @@ from django.shortcuts import render
 
 #Retrieve
 def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html", {})
+    obj = Tweet.objects.get(id=id) # GET from database
+    print(obj)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
 
 def tweet_list_view(request):
-    return render(request, "tweets/list_view.html", {})
+    queryset = Tweet.objects.all()
+    print(queryset)
+    for obj in queryset:
+        print(obj.content)
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
