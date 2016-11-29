578f8515e3eac5507d44dab77e87fd3b3f1d14cb
CFE <hello@teamcfe.com>
2016-11-29 15:23:47 -0800

    17 - Model Form

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import TweetModelForm
 from .models import Tweet
 
 
-admin.site.register(Tweet)
+#admin.site.register(Tweet)
+
+
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+    # class Meta:
+    #     model = Tweet
+        
+
+
+admin.site.register(Tweet, TweetModelAdmin)
---
+++ src/tweets/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+from .models import Tweet
+
+class TweetModelForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = [
+            "user",
+            "content" 
+        ]
+        #exclude = ['user']
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get("content")
+        if content == "abc":
+            raise forms.ValidationError("Cannot be ABC")
+        return content
