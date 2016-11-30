b32359fdbf836dd2cdf06739bcf928eb2191fb0a
CFE <hello@teamcfe.com>
2016-11-29 16:17:59 -0800

    22 - Delete View

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 
 from .views import (
     TweetCreateView,
+    TweetDeleteView,
     TweetDetailView,
+
     TweetListView,
     TweetUpdateView
     )
@@ -12,6 +14,7 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'), # /tweet/
     url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
-    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/
+    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
+    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
 ]
 
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,18 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+                CreateView,
+                DetailView,
+                DeleteView, 
+                ListView, 
+                UpdateView
+                )
 
 from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
 
-# Create
-
 class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
-    #login_url = '/admin/'
 
 
 class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
@@ -21,34 +25,23 @@ class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     template_name = 'tweets/update_view.html'
     success_url = "/tweet/"
 
-# Update
 
-#Delete
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy("home")
 
-#List / Search
 
-#Retrieve
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
-    #template_name = "tweets/detail_view.html"
-
-    # def get_object(self):
-    #     print(self.kwargs)
-    #     pk = self.kwargs.get("pk")
-    #     obj = get_object_or_404(Tweet, pk=pk)
-    #     return obj
-
 
 
 class TweetListView(ListView):
-    #template_name = "tweets/list_view.html"
     queryset = Tweet.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
-        #context["another_list"] = Tweet.objects.all()
-        #print(context)
         return context
 
 
@@ -60,14 +53,3 @@ def tweet_detail_view(request, pk=None): # pk == id
         "object": obj
     }
     return render(request, "tweets/detail_view.html", context)
-
-
-# def tweet_list_view(request):
-#     queryset = Tweet.objects.all()
-#     print(queryset)
-#     for obj in queryset:
-#         print(obj.content)
-#     context = {
-#         "object_list": queryset
-#     }
-#     return render(request, "tweets/list_view.html", context)
