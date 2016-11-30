43cf42952a6e9923c25ff175f5818c2cb2c33a1a
CFE <hello@teamcfe.com>
2016-11-29 16:33:18 -0800

    24 - Advanced Searching

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -38,7 +39,17 @@ class TweetDetailView(DetailView):
 
 
 class TweetListView(ListView):
-    queryset = Tweet.objects.all()
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                    Q(content__icontains=query) |
+                    Q(user__username__icontains=query)
+                    )
+        return qs
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
