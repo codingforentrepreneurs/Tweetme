74f25466f06b00c77767ed39db5a357948a538c5
CFE <hello@teamcfe.com>
2016-12-01 11:19:08 -0800

    51 - Signal Tests

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+from .models import UserProfile
+
+User = get_user_model()
+
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        self.username = "some_user"
+        new_user = User.objects.create(username=self.username)
+
+    def test_profile_created(self):
+        username = self.username
+        user_profile = UserProfile.objects.filter(user__username=self.username)
+        self.assertTrue(user_profile.exists())
+        self.assertTrue(user_profile.count() == 1)
+
+    def test_new_user(self):
+        new_user = User.objects.create(username=self.username + "abcsd")
