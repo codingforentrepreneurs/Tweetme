2c96e0d7d99f6b1c809231747c09381f475e796f
CFE <hello@teamcfe.com>
2016-12-01 10:54:27 -0800

    48 - Toggle Follow

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-    UserDetailView
+    UserDetailView,
+    UserFollowView
     )
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     # url(r'^search/$', TweetListView.as_view(), name='list'), # /tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'), # /tweet/1/
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+
+from django.views import View
 from django.views.generic import DetailView
 # Create your views here.
 
+from .models import UserProfile
 
 User = get_user_model()
 
@@ -15,3 +19,18 @@ class UserDetailView(DetailView):
                     User, 
                     username__iexact=self.kwargs.get("username")
                     )
+
+
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated():
+            user_profile, created = UserProfile.objects.get_or_create(user=request.user) # (user_obj, true)
+            if toggle_user in user_profile.following.all():
+                user_profile.following.remove(toggle_user)
+            else:
+                user_profile.following.add(toggle_user)
+        return redirect("profiles:detail", username=username)
+        # url = reverse("profiles:detail", kwargs={"username": username})
+        # HttpResponseRedirect(url)
