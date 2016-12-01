d0b02dc927eaac16650c30fe9cc03e9f2aed63ff
CFE <hello@teamcfe.com>
2016-11-30 17:50:22 -0800

    46 - User Profile & Followers

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import UserProfile
+
+
+admin.site.register(UserProfile)
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') # user.profile 
+    following   = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by') 
+    # user.profile.following -- users i follow
+    # user.followed_by -- users that follow me -- reverse relationship
+
+    def __str__(self):
+        return str(self.following.all().count())
