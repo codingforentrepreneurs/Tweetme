8c9cb55a8231e49781c75d2602c7e40ce9abda0a
CFE <hello@teamcfe.com>
2016-12-01 14:20:55 -0800

    57 - Retweet Part 4 with jQuery

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+    RetweetAPIView,
     TweetCreateAPIView,
     TweetListAPIView,
     )
@@ -11,6 +12,7 @@ urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")), 
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /tweet/create/
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet') #/api/tweet/id/tweet/
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,11 +1,29 @@
 from django.db.models import Q
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 from tweets.models import Tweet
 
 from .pagination import StandardResultsPagination
 from .serializers import TweetModelSerializer
+
+
+class RetweetAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, pk, format=None):
+        tweet_qs = Tweet.objects.filter(pk=pk)
+        message = "Not allowed"
+        if tweet_qs.exists() and tweet_qs.count() == 1:
+            #if request.user.is_authenticated():
+            new_tweet = Tweet.objects.retweet(request.user, tweet_qs.first())
+            if new_tweet is not None:
+                data = TweetModelSerializer(new_tweet).data
+                return Response(data)
+            message = "Cannot retweet the same in 1 day"
+        return Response({"message": message}, status=400)
 
 
 class TweetCreateAPIView(generics.CreateAPIView):
