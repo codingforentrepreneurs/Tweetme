1a052b6dc6a7e5716134571ab2c1ac53ac63e712
CFE <hello@teamcfe.com>
2016-12-01 14:44:09 -0800

    59 - Parse Username & Hashtag with Python Regex

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,11 +1,13 @@
+import re
+
 from django.conf import settings
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils import timezone
 
 # Create your models here.
-
 from .validators import validate_content
 
 
@@ -61,5 +63,31 @@ class Tweet(models.Model):
     #     if content == "abc":
     #         raise ValidationError("Content cannot be ABC")
     #     return super(Tweet, self).clean(*args, **kwargs)
+
+
+def tweet_save_receiver(sender, instance, created, *args, **kwargs):
+    if created and not instance.parent:
+        # notify a user
+        user_regex = r'@(?P<username>[\w.@+-]+)'
+        usernames = re.findall(user_regex, instance.content)
+        # send notification to user here.
+
+        hash_regex = r'#(?P<hashtag>[\w\d-]+)'
+        hashtags = re.findall(hash_regex, instance.content)
+        # send hashtag signal to user here.
+
+
+
+
+
+
+
+
+post_save.connect(tweet_save_receiver, sender=Tweet)
+
+
+
+
+
 
 
