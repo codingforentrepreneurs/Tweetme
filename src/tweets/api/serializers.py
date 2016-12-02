28e3b3772fd419ecf112011f426c698e34ecbca4
CFE <hello@teamcfe.com>
2016-12-02 12:31:52 -0800

    66 - Display Reply Tweet & Fix Likes

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -10,7 +10,9 @@ class ParentTweetModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer(read_only=True) #write_only
     date_display = serializers.SerializerMethodField()
     timesince = serializers.SerializerMethodField()
-    
+    likes = serializers.SerializerMethodField()
+    did_like = serializers.SerializerMethodField()
+
     class Meta:
         model = Tweet
         fields = [
@@ -20,8 +22,24 @@ class ParentTweetModelSerializer(serializers.ModelSerializer):
             'timestamp',
             'date_display',
             'timesince',
+            'likes',
+            'did_like',
+           
+
         ]
-    
+
+    def get_did_like(self, obj):
+        request = self.context.get("request")
+        user = request.user
+        if user.is_authenticated():
+            if user in obj.liked.all():
+                return True
+        return False
+
+
+    def get_likes(self, obj):
+        return obj.liked.all().count()
+
     def get_date_display(self, obj):
         return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
 
@@ -49,6 +67,7 @@ class TweetModelSerializer(serializers.ModelSerializer):
             'parent',
             'likes',
             'did_like',
+            'reply',
         ]
         
     def get_did_like(self, obj):
--- src/tweets/models.py
+++ src/tweets/models.py
@@ -56,6 +56,7 @@ class Tweet(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL)
     content     = models.CharField(max_length=140, validators=[validate_content])
     liked       = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked')
+    reply       = models.BooleanField(verbose_name='Is a reply?', default=False)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
